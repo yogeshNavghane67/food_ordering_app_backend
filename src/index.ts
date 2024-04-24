f89bfce0e89da2e67b783078163413e73f2a4df3
Yogesh Navghane <124075039+yogeshNavghane67@users.noreply.github.com>
2024-04-24 23:27:16 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+import express, { Request, Response } from "express";
+import cors from "cors";
+import "dotenv/config";
+import mongoose from "mongoose";
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=> console.log("connected to database!"));
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get("/test", async (req: Request, res: Response)=>{
+    res.json({ message: "Hello!" });
+});
+
+app.listen(7000, ()=> {
+    console.log("server started on localhost:7000")
+});
