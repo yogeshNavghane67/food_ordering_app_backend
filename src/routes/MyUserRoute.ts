dcdb2f5c6da191461b886fc49447697875a252d0
Yogesh Navghane <124075039+yogeshNavghane67@users.noreply.github.com>
2024-04-25 19:42:02 +0530

    user profile api

--- src/routes/MyUserRoute.ts
+++ src/routes/MyUserRoute.ts
@@ -1,20 +1,14 @@
 import express from "express";
 import MyUserController from "../controllers/MyUserController";
-// import MyUserController from "../controllers/MyUserController";
-// import { jwtCheck, jwtParse } from "../middleware/auth";
-// import { validateMyUserRequest } from "../middleware/validation";
+import { jwtCheck, jwtParse } from "../middleware/auth";
+import { validateMyUserRequest } from "../middleware/validation";
+
 
 const router = express.Router();
 
 // /api/my/user
-// router.get("/", jwtCheck, jwtParse, MyUserController.getCurrentUser);
-router.post("/", MyUserController.createCurrentUser);
-// router.put(
-//   "/",
-//   jwtCheck,
-//   jwtParse,
-//   validateMyUserRequest,
-//   MyUserController.updateCurrentUser
-// );
+//router.get("/", jwtCheck, jwtParse, MyUserController.getCurrentUser);
+router.post("/", jwtCheck, MyUserController.createCurrentUser);
+router.put("/", jwtCheck, jwtParse, validateMyUserRequest, MyUserController.updateCurrentUser);
 
 export default router;
