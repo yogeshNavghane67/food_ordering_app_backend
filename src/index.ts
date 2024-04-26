521eb5cead3428be22e84bb775f10444026b9473
Yogesh Navghane <124075039+yogeshNavghane67@users.noreply.github.com>
2024-04-27 01:59:35 +0530

    restaurant api

--- src/index.ts
+++ src/index.ts
@@ -3,19 +3,31 @@ import cors from "cors";
 import "dotenv/config";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
+import { v2 as cloudinary } from "cloudinary";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
 
-mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(()=> console.log("connected to database!"));
+
+mongoose
+  .connect(process.env.MONGODB_CONNECTION_STRING as string)
+  .then(() => console.log("connected to database!"));
+
+  cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET,
+  });
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 app.get("/health", async (req: Request, res: Response) => {
-    res.send({ message: "health OK!"});
-})
+  res.send({ message: "health OK!" });
+});
 
 app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 
-app.listen(7000, ()=> {
-    console.log("server started on localhost:7000")
+app.listen(7000, () => {
+  console.log("server started on localhost:7000");
 });
