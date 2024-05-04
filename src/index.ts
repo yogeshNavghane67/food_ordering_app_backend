faffccc350d493cfd5545a46cca40154bd11a2b2
Yogesh Navghane <yogeshnavghane661@gmail.com>
2024-05-04 23:42:16 +0530

    search api

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import mongoose from "mongoose";
 import myUserRoute from "./routes/MyUserRoute";
 import { v2 as cloudinary } from "cloudinary";
 import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import restaurantRoute from "./routes/RestaurantRoute";
+
 
 
 mongoose
@@ -27,6 +29,7 @@ app.get("/health", async (req: Request, res: Response) => {
 
 app.use("/api/my/user", myUserRoute);
 app.use("/api/my/restaurant", myRestaurantRoute);
+app.use("/api/restaurant", restaurantRoute);
 
 app.listen(7000, () => {
   console.log("server started on localhost:7000");
