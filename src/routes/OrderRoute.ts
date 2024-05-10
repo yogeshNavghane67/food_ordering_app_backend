07194136e6b586e6af06463b1cdb1bf050840837
Yogesh Navghane <yogeshnavghane661@gmail.com>
2024-05-11 02:16:16 +0530

    stripe cli with nodemon

--- src/routes/OrderRoute.ts
+++ src/routes/OrderRoute.ts
@@ -5,10 +5,12 @@ import OrderController from "../controllers/OrderController";
 const router = express.Router();
 
 router.post(
-    "/checkout/create-checkout-session",
-    jwtCheck,
-    jwtParse,
-    OrderController.createCheckoutSession
-  );
+  "/checkout/create-checkout-session",
+  jwtCheck,
+  jwtParse,
+  OrderController.createCheckoutSession
+);
+
+router.post("/checkout/webhook", OrderController.stripeWebhookHandler);
 
 export default router;
